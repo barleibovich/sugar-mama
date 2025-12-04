89ba82ca1537695cf68788e2add61adf3818b37c
Bar Leibovich <barleibovich@gmail.com>
2025-12-04 15:52:17 +0200

    vercel fix

--- src/main.tsx
+++ src/main.tsx
@@ -14,11 +14,21 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </AuthProvider>
   </React.StrictMode>
 );
+const ENABLE_SW = import.meta.env.VITE_ENABLE_SW === "true";
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").catch((err) => {
-      console.error("Service worker registration failed:", err);
-    });
+    navigator.serviceWorker.getRegistrations().then((regs) => regs.forEach((reg) => reg.unregister()));
+    if ("caches" in window) {
+      caches
+        .keys()
+        .then((keys) => keys.filter((key) => key.startsWith("sugarmama-cache")).forEach((key) => caches.delete(key)));
+    }
+
+    if (ENABLE_SW) {
+      navigator.serviceWorker.register("/service-worker.js").catch((err) => {
+        console.error("Service worker registration failed:", err);
+      });
+    }
   });
 }
